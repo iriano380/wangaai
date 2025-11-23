4674a6ad4952e6368d760d85edf31047f7de03fb
IrianoCreative Angecy <Xoonashop@gmail.com>
2025-11-23 18:53:01 +0200

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -28,7 +28,7 @@ async function UserOrLogin() {
 
       <div className="flex items-center font-semibold">
         <IconSeparator className="size-6 text-muted-foreground/50" />
-        <a href="https://www.binance.com/activity/referral-entry/CPA?ref=CPA_00PCY3US44">Binance btc</a>
+        <a href="https://www.binance.com/activity/referral-entry/CPA?ref=CPA_00PCY3US44"></a>
         <IconSeparator className="size-6 text-muted-foreground/50" />
         <a
   href="/new"
