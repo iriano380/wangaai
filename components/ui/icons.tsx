5404fa763fbec1be8fc15faea929fc29262669b8
IrianoCreative Angecy <Xoonashop@gmail.com>
2025-11-25 14:35:25 +0200

    Update icons.tsx

--- components/ui/icons.tsx
+++ components/ui/icons.tsx
@@ -167,12 +167,12 @@ function IconArrowDown({ className, ...props }: React.ComponentProps<'svg'>) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 256 256"
-      fill="currentColor"
+      viewBox="0 0 24 24"
+      fill="#FFFFFF"
       className={cn('size-4', className)}
       {...props}
     >
-      <path d="m205.66 149.66-72 72a8 8 0 0 1-11.32 0l-72-72a8 8 0 0 1 11.32-11.32L120 196.69V40a8 8 0 0 1 16 0v156.69l58.34-58.35a8 8 0 0 1 11.32 11.32Z" />
+      <path d="M1.94607 9.31543C1.42353 9.14125 1.4194 8.86022 1.95682 8.68108L21.043 2.31901C21.5715 2.14285 21.8746 2.43866 21.7265 2.95694L16.2733 22.0432C16.1223 22.5716 15.8177 22.59 15.5944 22.0876L11.9999 14L17.9999 6.00005L9.99992 12L1.94607 9.31543Z"></path>
     </svg>
   )
 }
